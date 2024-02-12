a14aa9a4ff56c7043c795d0195e620d795cd8185
Luiyi <luiyiantonyqp@gmail.com>
2024-02-12 11:42:59 -0500

    first commit

---
+++ tools/fat/fat.c
@@ -0,0 +1,77 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef uint8_t bool;
+#define true 1
+#define false 0
+
+typedef struct {
+  uint8_t BootJumpInstruction[3];
+  uint8_t OemIdentifier[8];
+  uint16_t BytesPerSector;
+  uint8_t SectorsPerCluster;
+  uint16_t ReservedSectors;
+  uint8_t FatCount;
+  uint16_t DirEntryCount;
+  uint16_t TotalSectors;
+  uint8_t MediaDescriptorType;
+  uint16_t SectorsPerFat;
+  uint16_t SectorsPerTrack;
+  uint16_t Heads;
+  uint32_t HiddenSectors;
+  uint32_t LargeSectorCount;
+
+  // extended boot record
+  uint8_t DriveNumber;
+  uint8_t _Reserved;
+  uint8_t Signature;
+  uint32_t VolumeId;
+  uint8_t VolumeLabel[11];
+  uint8_t SystemId[8];
+
+  // ... we don't care about code ...
+
+} __attribute__((packed)) BootSector;
+
+BootSector g_BootSector;
+uint8_t *g_Fat = NULL;
+
+bool readBootSector(FILE *disk) {
+  return fread(&g_BootSector, sizeof(g_BootSector), 1, disk) > 0;
+}
+
+bool readSectors(FILE *disk, uint32_t lba, uint32_t count, void *bufferOut) {
+  bool ok = true;
+  ok = ok && (fseek(disk, lba * g_BootSector.BytesPerSector, SEEK_SET == 0));
+  ok = ok &&
+       (fread(bufferOut, g_BootSector.BytesPerSector, count, disk) == count);
+  return ok;
+}
+
+bool readFat(FILE *disk) {
+  g_Fat = (uint8_t *)malloc(g_BootSector.SectorsPerFat *
+                            g_BootSector.BytesPerSector);
+  return readSectors(disk, g_BootSector.ReservedSectors,
+                     g_BootSector.SectorsPerFat, g_Fat);
+}
+
+int main(int argc, char **argv) {
+  if (argc < 3) {
+    printf("Syntax: %s <disk image> <file name>\n", argv[0]);
+    return -1;
+  }
+
+  FILE *disk = fopen(argv[1], "rb");
+  if (!disk) {
+    fprintf(stderr, "Cannot open dis image %s!", argv[1]);
+    return -1;
+  }
+
+  if (!readBootSector(disk)) {
+    fprintf(stderr, "COuld not read boot sector!\n");
+    return -2;
+  }
+
+  return 0;
+}
